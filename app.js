55aca2a5de0840c2d7bfbc96ca6d329ea100ae6f
erink0415 <erinekelly0415@yahoo.com>
2026-03-26 10:52:41 -0400

    finished assignment

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-var http = require("http");
-const port = process.env.PORT || 3001;
+const mongoose = require("mongoose");
 
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello World!");
-  })
-  .listen(port);
+//testdb is name of database, it will automatically make it
+mongoose
+  .connect("mongodb+srv://eek1_db_user:Fashiondiva0@reverie.0znkakq.mongodb.net/?appName=Reverie")
+  .then(() => console.log("Connected to mongodb..."))
+  .catch((err) => console.error("could not connect ot mongodb...", err));
+
+const schema = new mongoose.Schema({
+  name: String,
+});
+                                                                                 
+async function createMessage() {
+  const result = await message.save();
+  console.log(result);
+}
+
+//this creates a Message class in our app
+const Message = mongoose.model("Message", schema);
+const message = new Message({
+  name: "Hello World",
+});
+
+createMessage();
